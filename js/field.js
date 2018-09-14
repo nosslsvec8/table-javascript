0f6eb5d1dfe84929a984aee697f3a45bd6d7c4a7
nosslsvec8 <nosiljevets@gmail.com>
2018-09-14 11:56:38 +0300

    fix js buttons

--- js/field.js
+++ js/field.js
@@ -1,7 +1,3 @@
-// $(document).ready(function() {
-
-// });
-
 
 function action(action){
     if(action === 'deleteColumn'){
@@ -33,6 +29,12 @@ class Field{
         Div.className = "field__field-square";
         let is = document.getElementsByClassName("field__field-view-panel")[0];
         is.appendChild(Div);
+        console.log("+++");
+    }
+
+    deleteElement(){
+        document.getElementsByClassName("field__field-view-panel")[0].lastChild.remove();
+        console.log("---");
     }
 
     addColumn(){
@@ -40,32 +42,66 @@ class Field{
         for (let i = 0; i < this._row; i++) {
             this.addElement();
         }
+        if(this._column == 2){
+            document.getElementsByClassName('field__field-square_control-delete-column')[0].style.display="flex";
+        }
         this.updateField();
     }
 
     addRow(){
         this._row++;
+        for (let i = 0; i < this._column; i++) {
+            this.addElement();
+        }
+        if(this._row == 2){
+            document.getElementsByClassName('field__field-square_control-delete-row')[0].style.display="flex";
+        }
         this.updateField();
     }
 
     deleteColumn(){
+        if(this._column > 4){
+            for (let i = 0; i < this._row; i++) {
+                this.deleteElement();
+            }
+        }
+        else{
+            for (let i = 0; i < this._row * 2; i++) {
+                this.deleteElement();
+            }
+        }
         this._column--;
-        for (let i = 0; i < this._row * 2; i++) {
-            document.getElementsByClassName("field__field-view-panel")[0].lastChild.remove();
+        if(this._column == 1){
+            document.getElementsByClassName('field__field-square_control-delete-column')[0].style.display="none";
         }
         this.updateField();
     }
 
     deleteRow(){
+        if(this._row > 4){
+            for (let i = 0; i < this._column; i++) {
+                this.deleteElement();
+            }
+        }
+        else{
+            for (let i = 0; i < this._column * 2; i++) {
+                this.deleteElement();
+            }
+        }
         this._row--;
+        if(this._row == 1){
+            document.getElementsByClassName('field__field-square_control-delete-row')[0].style.display="none";
+        }
         this.updateField();
     }
 
     updateField(){
         this._updateWidth = sizeSquare * this._column + this._column * 3;
-        this._positioAddColumn = sizeSquare * this._column + this._column * 3 + 2;
+        this._positionAddColumn = sizeSquare * this._column + this._column * 3 + 2;
+        this._positionAddRow = sizeSquare * this._row + this._row * 2 + 5;
         document.getElementsByClassName('field__field-view-panel')[0].style.width = this._updateWidth + "px";
-        document.getElementsByClassName('field__field-square_control-add-row')[0].style.marginLeft = this._positioAddColumn + "px";
+        document.getElementsByClassName('field__field-square_control-add-column')[0].style.marginLeft = this._positionAddColumn + "px";
+        document.getElementsByClassName('field__field-square_control-add-row')[0].style.marginTop = this._positionAddRow + "px";
     }
 }
 
